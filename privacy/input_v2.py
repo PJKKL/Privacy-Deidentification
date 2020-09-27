4bac437caaeb22dca0c5ca9cf4d4495550df54e2
YOUNGBEEN, KIM <62539910+yb5464@users.noreply.github.com>
2020-09-27 18:49:13 +0900

    Update input_v2.py
    activate the codes
    
    issue
    -> 순차적으로 실행만 가능: 3차원 배열 선언이 불가능해서
    
    해결방법
    -> python numpy를 사용하면 될 것 같긴 하지만 그렇게까지 해야할 필요는 모르겠음

--- privacy/input_v2.py
+++ privacy/input_v2.py
@@ -23,22 +23,16 @@ if len(file_nms) == False:
     exit(1)
 else:
     print("해당 경로에 존재하는 파일 목록")
-    print(file_nms)
+    print(file_nms, end = "\n\n")
 
 data_arr = list()
-column = list()
-column_count = list()
 
-"""
-1. data_arr = csv 파일 데이터를 저장하는 배열. 3차원 리스트
-2. column = csv 파일의 첫번째 행을 저장하는 배열 -> 열의 이름. 2차원 리스트
-3. column_count = 열 이름의 개수를 저장하는 배열 -> 열 이름의 개수. 1차원 리스트
-"""
-
-for i in range(len(file_nms)):
+for f_name in file_nms:
     #추출 중인 파일 이름 표시
-    f_name = file_nms[i]
-    print(file_nms[i])
+    print(f_name, end = "\n\n")
+
+    #data배열을 초기화
+    data_arr = []
 
     #디렉토리 path 기준으로 모든 csv 파일들을 다 가져오기
     with open(abs_dir + f_name, 'r') as csvfile:
@@ -46,25 +40,26 @@ for i in range(len(file_nms)):
         #저장하는 temp 리스트 이름은 input
         rdf = csv.reader(csvfile)
         for line in rdf:
-            data_arr[i].append(line)
+            data_arr.append(line)
 
         #인풋들의 열 개수 파악
-        column.append(data_arr[i][0])
-        column_count.append(len(column[i]))
-        print("총 열 개수는 {}입니다.".format(column_count[i]))
-        print(column[i], end="\n\n")
+        column = data_arr[0]
+        column_count = len(column)
+        print("총 열 개수는 {}입니다.".format(column_count))
+        print(column, end="\n\n")
         #데이터 정제를 위해 첫 번째 열에 위치한 column name들을 분리
-        del data_arr[i][0]
+        del data_arr[0]
 
-
-
-        """
-        if len(data_arr) <= 1 and len(data_arr[0]):
+        #조건 수정 필요
+        if len(data_arr) <= 1:
             print("빈 csv파일입니다.")
         else: 
             print("전체 데이터 셋")
             for line in data_arr:
                 print(line)
-        """
+
+        print()
+
+
 
 
