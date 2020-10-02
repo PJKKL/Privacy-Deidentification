6f43f141d59dc229c86f6318e0ee4cf20bfa7ac8
YOUNGBEEN, KIM <62539910+yb5464@users.noreply.github.com>
2020-10-03 00:24:03 +0900

    Update generalization.py

--- privacy/generalization.py
+++ privacy/generalization.py
@@ -5,18 +5,18 @@ from random import randint
 
 
 # float유형을 반올림해서 int로 변경하는 함수
-def round_float_to_int(df):
+def df_round_float_to_int(df):
     return df.astype(int)
 
 # 일반 반올림 기법, df는 데이터 프레임, num은 자리 수
 # 0 -> 1의 자리, -1 -> 10의 자리
-def general_round(df, num):
+def df_round(df, num):
     df = df.round(num)
     return df
 
 # 일반 올림 기법, df는 데이터 프레임, num은 자리 수
 # 0 -> 1의 자리, -1 -> 10의 자리
-def general_ceil(df, num):
+def df_ceil(df, num):
     num_list = []
 
     for row in df:
@@ -26,7 +26,7 @@ def general_ceil(df, num):
 
 # 일반 내림 기법, df는 데이터 프레임, num은 자리 수
 # 0 -> 1의 자리, -1 -> 10의 자리
-def general_floor(df, num):
+def df_floor(df, num):
     num_list = []
 
     for row in df:
@@ -42,7 +42,7 @@ def df_min(df):
     return df.min(axis = 0, skipna = True)
 
 # random 라운딩
-def random_rounding(df):
+def df_random_rounding(df):
     temp_max = len(str(df_max(df)))
     temp_min = len(str(df_min(df)))
 
@@ -56,21 +56,19 @@ def random_rounding(df):
 
     op = randint(0, 2)
 
-    print(rand_num , op)
-
     if op == 0:
-        return general_round(df, -1 * rand_num)
+        return df_round(df, -1 * rand_num)
     elif op == 1:
-        return general_ceil(df, -1 * rand_num)
+        return df_ceil(df, -1 * rand_num)
     else:
-        return general_floor(df, -1 * rand_num)
+        return df_floor(df, -1 * rand_num)
 
 
 
 #def control_rounding():
 
 # 문자를 바꿔주는 replace 함수
-def char_data_categorisation(df):
+def df_char_replace(df):
     word_before = input("word before: ")
     word_after = input("word after: ")
     
@@ -78,7 +76,7 @@ def char_data_categorisation(df):
 
 
 # 마스킹 처리를 하는 함수       
-def masking(df):
+def df_masking(df):
     masking_start = int(input("masking start(nun): "))
     masking_end = int(input("masking end(num): "))
     masking_symbol = input("masking symbol: ")
@@ -103,9 +101,9 @@ def df_mapping(df):
     
     return df.replace(np.nan, key)
 
+
+
 # 함수 구현을 확인하기 위한 코드
 df = pd.read_csv('./dataset/dataset.csv', encoding = "EUC-KR")
-
-df['loc'] = df_mapping(df['loc'])
 
 print(df)
