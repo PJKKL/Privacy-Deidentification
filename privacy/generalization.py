73917b4ad43d9026e1b1dd64997758979c3dee87
YOUNGBEEN, KIM <62539910+yb5464@users.noreply.github.com>
2020-10-04 22:09:55 +0900

    Update generalization.py
    change in random_rounding
    and name of general rounding ceil floor

--- privacy/generalization.py
+++ privacy/generalization.py
@@ -5,18 +5,18 @@ from random import randint
 
 
 # float유형을 반올림해서 int로 변경하는 함수
-def df_round_float_to_int(df):
+def round_float_to_int(df):
     return df.astype(int)
 
 # 일반 반올림 기법, df는 데이터 프레임, num은 자리 수
 # 0 -> 1의 자리, -1 -> 10의 자리
-def df_round(df, num):
+def general_round(df, num):
     df = df.round(num)
     return df
 
 # 일반 올림 기법, df는 데이터 프레임, num은 자리 수
 # 0 -> 1의 자리, -1 -> 10의 자리
-def df_ceil(df, num):
+def general_ceil(df, num):
     num_list = []
 
     for row in df:
@@ -26,7 +26,7 @@ def df_ceil(df, num):
 
 # 일반 내림 기법, df는 데이터 프레임, num은 자리 수
 # 0 -> 1의 자리, -1 -> 10의 자리
-def df_floor(df, num):
+def general_floor(df, num):
     num_list = []
 
     for row in df:
@@ -36,36 +36,27 @@ def df_floor(df, num):
 
 
 # random rounding 구현을 위해서 끌어온 함수
-def df_max(df):
-    return df.max(axis = 0, skipna = True)
 def df_min(df):
     return df.min(axis = 0, skipna = True)
 
 # random 라운딩
 def df_random_rounding(df):
-    temp_max = len(str(df_max(df)))
     temp_min = len(str(df_min(df)))
 
-    print(temp_max, temp_min)
-    print(type(temp_max))
+    #랜덤한 자리수를 구함 (0부터 가장 작은 값의 자리 수 까지)
+    #예를 들어 100, 230, 1000, 12349, 203 이라는 데이터가 들어있다면 최소 값 100의 자리수(3)이 temp_min으로 들어감
+    #따라서 라운딩 조치를 취해줄 값은 0 ~ 2 사이에서 랜덤하게 선택되어 rand_num으로 들어감
+    rand_num = randint(0, temp_min - 1) 
 
-    if temp_max == temp_min:
-        rand_num = randint(0, temp_max)
-    else:
-        rand_num = randint(temp_min, temp_max)
-
+    #어떤 라운딩 조치를 취할 것인지를 골라주는 랜덤 플래그
     op = randint(0, 2)
 
     if op == 0:
-        return df_round(df, -1 * rand_num)
+        return general_round(df, -1 * rand_num)
     elif op == 1:
-        return df_ceil(df, -1 * rand_num)
+        return general_ceil(df, -1 * rand_num)
     else:
-        return df_floor(df, -1 * rand_num)
-
-
-
-#def control_rounding():
+        return general_floor(df, -1 * rand_num)
 
 # 문자를 바꿔주는 replace 함수
 def df_char_replace(df):
